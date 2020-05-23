56d68ca0caed97c39d9e09f6f40d23e13d11890e
Abel Lifaefi Mbula <abelmbula@protonmail.com>
2020-05-23 21:34:19 +0200

    feat: add side nav component

--- src/components/container.js
+++ src/components/container.js
@@ -3,6 +3,14 @@ import React from "react"
 import containerStyles from "./container.module.css"
 
 export default ({ children }) => (
-    <div className={containerStyles.container}>{children}</div>
+    <div 
+        className={containerStyles.container}
+        style={{
+            // border: `2px solid red`,
+            
+        }}
+    >
+        {children}
+    </div>
 )
 
---
+++ src/components/verticalMenu.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import './verticalMenu.css'
+
+export default () => {
+    
+    return (
+        <nav 
+            className="side_nav" 
+            style={{
+                position: `-webkit-sticky`,
+                position: `sticky`,
+                bottom: `42vh`,
+                right: `-20vw`,
+                width: `150px`,
+                zIndex: `100`
+
+            }}
+            onScroll={() => handleScroll}
+        >
+            <ul style={{
+                listStyle: `none`,
+            }}>
+                
+                <li><Link to="/#intro">About </Link> 
+                </li>
+                <li><Link to="/#work">Work</Link></li>
+                <li><Link to="/#projects">Projects</Link></li>
+
+            </ul>
+        </nav>
+    )
+}
